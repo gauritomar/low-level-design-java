3c4c41ed67ba26384fdb44a7e82309ba90762910
Gauri <gaurisinghh3@gmail.com>
2023-03-24 04:01:03 +0530

    Create strings12.java

---
+++ java-revision-oops/data-structures/strings12.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class strings12 {
+    
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is: " + name);
+    }
+}
